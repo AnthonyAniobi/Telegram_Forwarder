958784fbb2b19c3fd94d376b807451f2a2194fed
AnthonyAniobi <anthonyaniobi198@gmail.com>
2021-07-12 01:13:11 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+from decouple import config
+
+api_hash = config('API_HASH')
+api_id = config('API_ID')
+
+input_chat_id = config('INPUT_CHAT_ID')
+output_chat_id = config('OUTPUT_CHAT_ID')
---
+++ main.py
@@ -0,0 +1,5 @@
+from telethon import TelegramClient, events, sync
+from config import api_hash, api_id, input_chat_id, output_chat_id
+
+client = TelegramClient('', api_id, api_hash)
+
